86de4ba8a294f38cddec515cbe8e5f96e8c754ca
Harshit <hsompura2012@gmail.com>
2025-12-04 15:18:10 +0530

    Update

--- main.tf
+++ main.tf
@@ -8,21 +8,6 @@ resource "aws_instance" "instance_launch" {
     Name = each.key
   }
 
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      password = "DevOps321"
-      host     = self.private_ip
-    }
-    inline =  [
-      "sudo dnf install python3.13-pip -y",
-      "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/hsompura2012-web/Practice-Ansible-template-V1.git main.yaml -e component=${each.key} -e env=dev"
-
-]
-  }
-
 }
 
 resource "aws_route53_record" "Record_Launch" {
@@ -32,4 +17,27 @@ resource "aws_route53_record" "Record_Launch" {
   type    = "A"
   ttl     = 50
   records = [aws_instance.instance_launch[each.key].private_ip]
+}
+
+resource "null_resource" "Instance_null" {
+  depends_on = [
+
+
+  ]
+
+  for_each = var.Instance_Det
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.instance_launch[each.key].private_ip
+    }
+    inline =  [
+      "sudo dnf install python3.13-pip -y",
+      "sudo pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/hsompura2012-web/Practice-Ansible-template-V1.git main.yaml -e component=${each.key} -e env=dev"
+
+    ]
+  }
 }
